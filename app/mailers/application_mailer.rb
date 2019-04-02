489db4d7842c41805b2cbce583fd7be9bc98ce88
plapicola <p.lapicola@gmail.com>
2019-04-02 11:06:28 -0600

    Adds additional gems and sets up rspec
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>
    Co-authored-by: Tim Allen <42568362+timnallen@users.noreply.github.com>
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Class for common mailer actions
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Class for common ActiveRecord methods
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
