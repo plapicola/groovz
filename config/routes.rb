f83d79d0ee783fcf363badffbbf0ec579db49eba
plapicola <p.lapicola@gmail.com>
2019-04-02 22:56:47 -0600

    Adds correction for OAuth path helper naming

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'parties#index'
 
   get '/login', to: 'sessions#new', as: :login
-  get '/auth/spotify', as: :spotify_oath
+  get '/auth/spotify', as: :spotify_oauth
   get '/auth/spotify/callback', to: 'sessions#create', as: :spotify_callback
 end
