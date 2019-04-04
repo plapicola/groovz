4aa8eddd6f2a7dd83ddaea7dfb420697e2103e46
Scott Thomas <31359242+smthom05@users.noreply.github.com>
2019-04-04 13:16:03 -0600

    Update parties_controller.rb
    Fixed typo

--- app/controllers/host/parties_controller.rb
+++ app/controllers/host/parties_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Host
-  # Controller for displaing party creation and show for host users
+  # Controller for displaying party creation and show for host users
   class PartiesController < ApplicationController
     def new; end
   end
