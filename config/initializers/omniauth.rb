a5d9f5662f5108582ad082d9c721d8e07cf24a0b
plapicola <p.lapicola@gmail.com>
2019-04-06 09:35:22 -0600

    Adds scopes to omniauth, user column to party table, and has_many artists to user model.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'],
-  scope: "user-read-playback-state"
+  scope: %w[user-read-playback-state
+            user-top-read
+            playlist-modify-public
+            user-modify-playback-state].join(" ")
 end
