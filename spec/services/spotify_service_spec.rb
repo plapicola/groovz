71e1ddcaa3c7230e95f7a297a17b250f21d0c52f
plapicola <p.lapicola@gmail.com>
2019-04-07 18:58:24 -0600

    Adds service spec for spotify service and test for request_devices instance method
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

---
+++ spec/services/spotify_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe SpotifyService do
+  it 'exists' do
+    user = create(:user)
+    service = SpotifyService.new(user)
+
+    expect(service).to be_a SpotifyService
+  end
+
+  describe 'instance methods' do
+    describe '.request_devices' do
+      it 'returns a list of available devices for the user' do
+        user = create(:user)
+        service = SpotifyService.new(user)
+
+        devices = service.request_devices
+
+        expect(devices).to be_a Array
+        expect(devices[0]).to have_key :id
+        expect(devices[0]).to have_key :name
+      end
+    end
+  end
+end
