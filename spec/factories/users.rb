e675807a7ae7b3c6e91d57f5cbac116252dd5382
Tim Allen <tnelsallen3@gmail.com>
2019-04-09 17:27:41 -0600

    Adds class method specs for party

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     uid { ENV['SPOTIFY_UID'] }
     token { ENV['SPOTIFY_TOKEN'] }
     refresh_token { ENV['SPOTIFY_REFRESH_TOKEN'] }
-    expires_at { 1554852454 }
+    expires_at { 1555852454 }
     expires { true }
     name { 'Tim' }
   end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Party, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :code }
+    it { should validate_uniqueness_of :code }
+    it { should validate_presence_of :user_id }
+  end
+
+  describe 'relationships' do
+    it { should have_many :party_tracks }
+    it { should have_many :users }
+    it { should belong_to :user }
+  end
+
+  describe 'class methods' do
+    it 'generate_party' do
+      user = create(:user)
+      party_count = Party.all.count
+      expected_party_count = party_count + 1
+
+      Party.generate_party(user.id)
+      expect(Party.all.count).to eq(expected_party_count)
+    end
+
+    it 'get_party_tastes' do
+      user_1 = create(:user, tempo: 60.0,
+                             mode: 0.0,
+                             valence: 0.0,
+                             energy: 0.0,
+                             danceability: 0.0,
+                             acousticness: 0.0
+                           )
+
+      user_2 = create(:user, tempo: 120.0,
+                             mode: 1.0,
+                             valence: 1.0,
+                             energy: 1.0,
+                             danceability: 1.0,
+                             acousticness: 1.0,
+                             uid: '126311576'
+                           )
+      party = Party.create(user: user_1, users: [user_1, user_2], code: 'sample')
+      party_with_tastes = Party.get_party_tastes(party)
+
+      expect(party_with_tastes.avg_mode).to eq(0.50)
+      expect(party_with_tastes.avg_valence).to eq(0.50)
+      expect(party_with_tastes.avg_dance).to eq(0.50)
+      expect(party_with_tastes.avg_acoust).to eq(0.50)
+      expect(party_with_tastes.avg_energy).to eq(0.50)
+      expect(party_with_tastes.avg_tempo).to eq(90.0)
+    end
+  end
+
+  describe 'instance methods' do
+    it 'new_song?' do
+      
+    end
+  end
 end
