bc9f2919438f76e67a7105fc238b78ee06f14415
plapicola <p.lapicola@gmail.com>
2019-04-09 23:24:32 -0600

    Adds api controller action to handle playback

--- app/controllers/api/v1/me/tracks_controller.rb
+++ app/controllers/api/v1/me/tracks_controller.rb
@@ -2,8 +2,19 @@ module Api
   module V1
     module Me
       class TracksController < ApplicationController
+        skip_forgery_protection
+
         def show
           render json: PartyTrackSerializer.new(current_user.party&.current_song)
+        end
+
+        def update
+          if user_owns_party
+            PlaylistSpotifyService.new(current_user).start_playback
+            head :ok
+          else
+            head :not_authorized
+          end
         end
       end
     end
