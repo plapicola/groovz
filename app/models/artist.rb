cd171cd16793ee918dafd95c4c5ba356399d6a45
Tim Allen <tnelsallen3@gmail.com>
2019-04-09 22:06:54 -0600

    Adds artist models tests

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,6 +3,9 @@
 class Artist < ApplicationRecord
   belongs_to :user
 
+  validates_presence_of :spotify_id
+  validates_presence_of :user_id
+
   def self.get_common_artists(party)
     Artist.select('artists.spotify_id, COUNT(artists.user_id) AS frequency')
                     .joins(user: :party)
