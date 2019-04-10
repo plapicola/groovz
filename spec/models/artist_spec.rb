3246ba17bdb41b2a4c50bad43c8a7e61b9216e30
plapicola <p.lapicola@gmail.com>
2019-04-10 10:50:13 -0600

    Corrects merge conflict results
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>
    Co-authored-by: Tim Allen <42568362+timnallen@users.noreply.github.com>
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Artist type: :model do
+RSpec.describe Artist, type: :model do
   describe 'validations' do
     it { should validate_presence_of :spotify_id }
     it { should validate_presence_of :user_id }
