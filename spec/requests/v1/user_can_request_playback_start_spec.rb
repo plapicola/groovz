769bb35ac170e6f2e6f87058f44df326d4ebe1c7
plapicola <p.lapicola@gmail.com>
2019-04-09 23:12:42 -0600

    Corrects issues in test

--- spec/requests/v1/user_can_request_playback_start_spec.rb
+++ spec/requests/v1/user_can_request_playback_start_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'Playback API' do
       user = create(:user)
       party = create(:party,
         playlist_id: '4ZenWelYqdG7lVhcDerX9D',
-        device_id: '6f5333c011e9913b7ea319fdcc9f144e7e4329f2'
-        user: user
-        users: [:user]
+        device_id: '6f5333c011e9913b7ea319fdcc9f144e7e4329f2',
+        user: user,
+        users: [user]
       )
 
       get '/api/v1/me/start_playback'
