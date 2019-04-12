1eab3f5ca0d47cba8f554073b366fc3c7e758f29
plapicola <p.lapicola@gmail.com>
2019-04-12 09:03:06 -0600

    Updates styling for host view to prevent text misplacement, and adds conditional to stop party update after 24 hours

--- app/jobs/query_current_playing_job.rb
+++ app/jobs/query_current_playing_job.rb
@@ -3,13 +3,15 @@
 class QueryCurrentPlayingJob < ApplicationJob
   queue_as :default
   after_perform do |job|
-    self.class.set(wait: 5.seconds).perform_later(job.arguments.first) if @party
+    self.class.set(wait: ENV['SONG_POLLING_FREQUENCY'].to_i.seconds).perform_later(job.arguments.first) if active?
   end
 
   def perform(id)
-    current_playback = party(id).playing
-    broadcast_song
-    broadcast_playback(id, current_playback)
+    if party(id)
+      current_playback = @party.playing
+      broadcast_song
+      broadcast_playback(id, current_playback)
+    end
   end
 
   private
@@ -35,5 +37,12 @@ class QueryCurrentPlayingJob < ApplicationJob
 
   def party(id)
     @party ||= Party.find_by_id(id)
+  end
+
+  private
+
+  def active?
+    # Temporary workaround to stop job running perpetually
+    @party && @party.created_at > 1.day.ago
   end
 end
